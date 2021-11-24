0b0bc21513c218605fb9b35c98eacaca7dcc9e3a
ministrov <61511030+ministrov@users.noreply.github.com>
2021-11-25 00:50:09 +0300

    Almost did a module1-task3

--- src/main.js
+++ src/main.js
@@ -1,7 +1,20 @@
 import { createUserRankTemplate } from "./view/user-rank.js";
 import { renderTemplate, RenderPosition } from "./render.js";
+import { createMainNavMenuTemplate } from "./view/main-nav-menu.js";
+import { createSortListTemplate } from "./view/sorting.js";
+import { createFilmCardTemplate } from "./view/film-card.js";
+import { createShowMoreBtnTemplate } from "./view/show-more-btn.js";
 
 const headerEl = document.querySelector('.header');
+const mainEl = document.querySelector('.main');
+const FILM_QUANTITY_CARD = 5;
 
 renderTemplate(headerEl, createUserRankTemplate(), RenderPosition.BEFOREEND);
-console.log(createUserRankTemplate());
+renderTemplate(mainEl, createMainNavMenuTemplate(), RenderPosition.BEFOREEND);
+renderTemplate(mainEl, createSortListTemplate(), RenderPosition.BEFOREEND);
+
+for (let i = 0; i < FILM_QUANTITY_CARD; i++) {
+    renderTemplate(mainEl, createFilmCardTemplate(), RenderPosition.BEFOREEND);
+}
+
+renderTemplate(mainEl, createShowMoreBtnTemplate(), RenderPosition.BEFOREEND);
