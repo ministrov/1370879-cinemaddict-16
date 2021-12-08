53e6472138a76ad88d5d5b1b353fe3e32b969e26
ministrov <61511030+ministrov@users.noreply.github.com>
2021-12-08 14:06:14 +0300

    Edited all the bugs about rendering

--- src/main.js
+++ src/main.js
@@ -6,65 +6,58 @@ import FilmCard from './view/film-card.js';
 import ShowMoreBtn from './view/show-more-btn.js';
 import FilmsBoard from './view/films-board.js';
 import FooterStats from './view/footer-stats.js';
-import FilmPopup from './view/film-details-popup.js';
+// import FilmPopup from './view/film-details-popup.js';
 import { generateFilm } from './mock/film.js';
 
 const FILM_QUANTITY_CARD = 15;
 const MAX_FILMS = 5;
-
 const headerEl = document.querySelector('.header');
 const mainEl = document.querySelector('.main');
-
 const films = Array.from({length: FILM_QUANTITY_CARD}, generateFilm);
-
 let currentCount = MAX_FILMS;
 const filmsBoardComp = new FilmsBoard();
 const filmsBoardEl = filmsBoardComp.element.querySelector('.films-list__container');
+const footer = document.querySelector('.footer');
+const footerStats = footer.querySelector('.footer__statistics');
 
 render(headerEl, new UserRank().element, RenderPosition.BEFOREEND);
 render(mainEl, new MainNavigation(films).element, RenderPosition.BEFOREEND);
 render(mainEl, new SortList().element, RenderPosition.BEFOREEND);
-// render(filmsBoardEl, new FilmCard().element, RenderPosition.BEFOREEND);
+render(mainEl, filmsBoardComp.element, RenderPosition.BEFOREEND);
+render(mainEl, new ShowMoreBtn().element, RenderPosition.BEFOREEND);
+render(footerStats, new FooterStats().element, RenderPosition.BEFOREEND);
 
-const footer = document.querySelector('.footer');
-const footerStats = footer.querySelector('.footer__statistics');
-
-const renderFilms = (count) => {
+const renderFilmCard = (count) => {
   filmsBoardEl.innerHTML = '';
 
   for (let i = 0; i < count; i++) {
     render(filmsBoardEl, new FilmCard(films[i]).element, RenderPosition.BEFOREEND);
   }
-
-  // render(mainEl, filmsBoardComp.element, RenderPosition.BEFOREEND);
 };
 
-const renderFilm = (filmBoardElement, film) => {
-  const filmComponent = new FilmCard(film);
-  const filmPopupComponent = new FilmPopup(film);
+// const renderFilm = (filmBoardElement, films) => {
+//   const filmComponent = new FilmCard(films);
+//   const filmPopupComponent = new FilmPopup(films);
 
-  const replaceFilmCardToFilmPopup = () => {
-    filmBoardElement.replaceChild(filmPopupComponent.element, filmComponent.element);
-  };
+//   const replaceFilmCardToFilmPopup = () => {
+//     filmBoardElement.replaceChild(filmPopupComponent.element, filmComponent.element);
+//   };
 
-  const replaceFilmPopupToFilmCard = () => {
-    filmBoardElement.replaceChild(filmComponent.element, filmPopupComponent.element);
-  };
+//   const replaceFilmPopupToFilmCard = () => {
+//     filmBoardElement.replaceChild(filmComponent.element, filmPopupComponent.element);
+//   };
 
-  filmComponent.element.querySelector('.film-card__link').addEventListener('click', () => {
-    replaceFilmCardToFilmPopup();
-  });
+//   filmComponent.element.querySelector('.film-card__link').addEventListener('click', () => {
+//     replaceFilmCardToFilmPopup();
+//   });
 
-  filmPopupComponent.element.querySelector('.film-details__close-btn').addEventListener('click', () => {
-    replaceFilmPopupToFilmCard();
-  });
-  render(mainEl, filmPopupComponent.element, RenderPosition.BEFOREEND);
-};
+//   filmPopupComponent.element.querySelector('.film-details__close-btn').addEventListener('click', () => {
+//     replaceFilmPopupToFilmCard();
+//   });
+//   render(filmBoardElement, filmPopupComponent.element, RenderPosition.BEFOREEND);
+// };
 
-renderFilms(MAX_FILMS);
-
-render(mainEl, new ShowMoreBtn().element, RenderPosition.BEFOREEND);
-render(footerStats, new FooterStats().element, RenderPosition.BEFOREEND);
+renderFilmCard(MAX_FILMS);
 
 const moreButton = document.querySelector('.films-list__show-more');
 
@@ -76,7 +69,7 @@ const onMoreButtonClick = () => {
     moreButton.removeEventListener('click', onMoreButtonClick);
   }
 
-  renderFilm(currentCount);
+  renderFilmCard(currentCount);
 };
 
 moreButton.addEventListener('click', onMoreButtonClick);
