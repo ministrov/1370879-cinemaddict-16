e4df2576150e23e6890b3931612e24c493897a09
ministrov <61511030+ministrov@users.noreply.github.com>
2021-12-24 18:38:11 +0300

    trying to solve the presenter problem but could not

--- src/presenter/MovieListPresenter.js
+++ src/presenter/MovieListPresenter.js
@@ -1,33 +1,44 @@
 import NoFilmCard from '../view/no-film-card';
 import FilmsBoard from '../view/films-board';
 import { render, RenderPosition } from '../render';
-import { generateFilm } from '../mock/film';
 import MoviePresenter from '../presenter/MoviePresenter';
 
 const filmsBoardComp = new FilmsBoard();
 const filmsBoardEl = filmsBoardComp.element.querySelector('.films-list__container');
-const FILM_QUANTITY_CARD = 15;
-const films = Array.from({length: FILM_QUANTITY_CARD}, generateFilm);
-const MAX_FILMS = 5;
+// const FILM_QUANTITY_CARD = 15;
+// const MAX_FILMS = 5;
 export default class MovieListPresenter {
   #filmsContainer = null;
   #noFilmCardComponent = new NoFilmCard();
 
-  constructor(filmsContainer) {
+  constructor(filmsContainer, films) {
+    this.films = films;
     this.#filmsContainer = filmsContainer;
+  }
+
+  init() {
+
   }
 
   #renderBoard = () => {
     render(this.#filmsContainer, filmsBoardComp, RenderPosition.BEFOREEND);
   }
 
-  #renderFilmCards = () => {
+  #renderFilmCards = (count) => {
     filmsBoardEl.innerHTML = '';
-    if (films.length === 0) {
+    if (this.films.length === 0) {
       render(filmsBoardEl,  this.#noFilmCardComponent, RenderPosition.BEFOREEND);
     } else {
-      MoviePresenter.renderFilmCard(MAX_FILMS);
+      for (let i = 0; i < count; i++) {
+        const moviePresenter = new MoviePresenter();
+        // const filmCard = new FilmCard(films[i]);
+        render(filmsBoardEl, moviePresenter, RenderPosition.BEFOREEND);
+        moviePresenter.showCardPopup();
+      }
     }
   }
+
+  // MoreBtn
+  // Фильтрация фильмов
 }
 
--- src/presenter/MoviePresenter.js
+++ src/presenter/MoviePresenter.js
@@ -1,24 +1,18 @@
-import FilmCard from '../view/film-card';
-import FilmsBoard from '../view/films-board';
-import { render, RenderPosition } from '../render';
-import { onFilmCardClick } from '../view/main';
-import { generateFilm } from '../mock/film';
-
-const filmsBoardComp = new FilmsBoard();
-const filmsBoardEl = filmsBoardComp.element.querySelector('.films-list__container');
-const FILM_QUANTITY_CARD = 15;
-const films = Array.from({length: FILM_QUANTITY_CARD}, generateFilm);
 export default class MoviePresenter {
+  #film = null;
+
   constructor(film) {
-    this.film = film;
+    this.#film = film;
   }
 
-  #renderFilmCard = (count) => {
-    filmsBoardEl.innerHTML = '';
-    for (let i = 0; i < count; i++) {
-      const filmCard = new FilmCard(films[i]);
-      render(filmsBoardEl, filmCard, RenderPosition.BEFOREEND);
-      filmCard.showCardPopup(onFilmCardClick);
-    }
-  }
+  // init() {
+
+  // }
+
+  // #renderFilmCard = () => {
+  //   filmsBoardEl.innerHTML = '';
+  // }
+
+  // Popup
+  // Обработка событий
 }
